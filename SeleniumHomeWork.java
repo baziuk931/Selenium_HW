0da170613314ed3b2b786babb07f4f4552b37246
Iryna Baziuk <ibazi@softserveinc.com>
2020-01-23 22:56:55 +0200

    added comment'

--- SeleniumHomeWork.java
+++ SeleniumHomeWork.java
@@ -18,6 +18,7 @@ public class SeleniumHomeWork {
 		ChromeDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		//new method
 		driver.navigate().to(testUrl);
 		String mainWindow = driver.getWindowHandle();
 		firstWindowTitle = driver.getTitle();
